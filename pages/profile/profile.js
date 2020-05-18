b6af9ecffb2a1aa2343e99e7ce0961dde02d12df
lz-126 <bpmf2@outlook.com>
2020-05-18 16:20:15 +0800

    1-初始小程序

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,33 +1,11 @@
-// pages/home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name: 'Coderwhy',
-    age: 18,
-    students: [
-      { id: 110, name: 'kobe', age: 30 },
-      { id: 111, name: 'james', age: 28 },
-      { id: 112, name: 'curry', age: 32 },
-      { id: 113, name: 'why', age: 18 }
-    ],
-    counter: 0
-  },
-  addClick() {
-    //错误写法
-    // this.data.counter+=1
 
-    //写法与react类似
-    this.setData({
-      counter: this.data.counter + 1
-    })
-  },
-  subClick() {
-    this.setData({
-      counter: this.data.counter - 1
-    })
   },
 
   /**
