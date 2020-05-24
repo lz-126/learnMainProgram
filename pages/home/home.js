d494a48c0d0f28b2cde4c8d63a259c1492f94241
lz-126 <bpmf2@outlook.com>
2020-05-24 20:52:38 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,65 @@
 // pages/home/home.js
+//导入请求
+  import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  
+  onLoad:function(options){
+    //1.原生方式发送网络请求
+    this.get_data_origin()
+    //使用封装的request方法获取网络请求
+    //防止出现回调地狱
+    request({
+      url:"http://httpbin.org"
+    }).then(res=>{
+      console.log(res);
+      
+    }).catch(err=>{
+      console.log(err);
+      
+    })
+  
+},
+ 
+  get_data_origin(){
+      //发送网络请求
+      //01-最简单的get请求
+      // wx.request({
+      //   url:'http://httpbin.org',
+      //   success:function(res){
+      //     console.log(res);
+      //   }
+      // })
+      //02-git请求 携带参数
+      // wx.request({
+      //   url: 'http://httpbin.org',
+      //   data:{
+      //     name:"codeWhy",
+      //     age:18
+      //  },
+      //  success:function(res){
+      //   console.log(res);
+      //  }
+      // })
+      // 03- post请求 并携带参数
+      // wx.request({
+      //   url: 'http://httpbin.org/post',
+      //   method:'post',  
+      //   data:{
+      //     name:"codeWhy",
+      //     age:19
+      //   },
+      //   success:function(res){
+      //     console.log(res);
+      //   },
+      //   fail:function(err){
+      //     console.log(err);
+          
+      //   }
+      // })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  //
 })
